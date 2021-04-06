1219c6c9a95175da7cc38e55829c45e4bdcf9dc5
谭树杰 <tanshujie@ainnovation.com>
2021-04-06 08:42:26 +0800

    format quicksort

--- 91_items/ch1_引论/1_理解Pythonic概念/1_quicksort.py
+++ 91_items/ch1_引论/1_理解Pythonic概念/1_quicksort.py
@@ -1,15 +1,17 @@
 def quicksort(array):
-  less = []; greater = []
-  if len(array) <= 1:
-    return array
-  pivot = array.pop()
-  
-  for x in array:
-    if x <= pivot:
-      less.append(x)
-    else:
-      greater.append(x)
-  return quicksort(less) + [pivot] + quicksort(greater)
+    less = []
+    greater = []
+    if len(array) <= 1:
+        return array
+    pivot = array.pop()
+
+    for x in array:
+        if x <= pivot:
+            less.append(x)
+        else:
+            greater.append(x)
+    return quicksort(less) + [pivot] + quicksort(greater)
+
 
 if __name__ == '__main__':
-  print(quicksort([9, 4, 7, 3]))
+    print(quicksort([9, 4, 7, 3]))
